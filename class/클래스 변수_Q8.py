35d16a51ed7c0f7246d8c9f5996dba43b2d474af
5h6m <5h6sm.github@gmail.com>
2022-11-03 09:50:40 +0900

    클래스 변수_Q8 주석 추가

--- class/클래스 변수_Q8.py
+++ class/클래스 변수_Q8.py
@@ -1,6 +1,6 @@
 class MyClass:
     # 클래스 변수
-    a_var = 1000
+    a_var = 1000  # 디폴트가 없기 때문에 꼭 수를 정해줄 것
 
     # 클래스 내 메서드
     def a_method(self):
@@ -17,7 +17,7 @@ print('[2] 클래스 변수의 초깃값을 3000으로 변경 ---> ', MyClass.a_
 # 클래스 내 메서드 호출을 통해서 변수값을 수정
 m1 = MyClass()
 m1.a_method()
-print('[3] 클래스 내 메서드 호출을 통해서 변수값을 수정 --->', MyClass.a_var)
+print('[3] 클래스 내 메서드 호출을 통해서 변수값을 수정 --->', MyClass.a_var)  # m1.a_var이 아님을 주의
 
 # 클래스 메서드 호출을 통해서 변수값 수정
 # MyClass.a_method() #ERROR
